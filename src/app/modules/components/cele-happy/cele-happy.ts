d5f6d7c80ec41345173df75d0daffce47835fc22
karanveerSsingh <karanveer0508singh@gmail.com>
2026-02-08 01:35:07 +0530

    puss

--- src/app/modules/components/cele-happy/cele-happy.ts
+++ src/app/modules/components/cele-happy/cele-happy.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { Lines } from '../lines/lines';
 
 @Component({
   selector: 'app-cele-happy',
-  imports: [Lines],
   templateUrl: './cele-happy.html',
   styleUrl: './cele-happy.scss',
 })
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 import { App } from './app/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-
 
 bootstrapApplication(App, {
   providers: [
